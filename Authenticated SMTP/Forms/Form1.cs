b32757191876345aded1129c5341f910a691d581
Andrew Stobart <astobart@microsoft.com>
2022-08-02 14:30:48 -0500

    Nuget package updates.

--- Authenticated SMTP/Forms/Form1.cs
+++ Authenticated SMTP/Forms/Form1.cs
@@ -55,7 +55,7 @@
  *
  *  2.1.1 (August 2, 2022)   - .Net updated from 4.6 to 4.8
  *                           - Updated MailKit and MimeKit from 2.10.1 to 3.3.0
- *                           - Added a sentance to the About page noting that the application only supports Basic Auth and not Modern Auth
+ *                           - Added a sentence to the About page noting that the application only supports Basic Auth and not Modern Auth
  *
  *----------------------------------------------------------------------------*/
 
